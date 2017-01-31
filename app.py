77b13e28c96ffdacd66c75f640ca3ecfa3b3e0d6
Zechariah Schneider <zechsch@umich.edu>
2017-01-31 18:55:35 -0500

    changed db connection

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/btl'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 heroku = Heroku(app)
 
