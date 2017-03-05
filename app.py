a1405f267a6f12396dfa4b0f6aa09b22a1c58c06
Zechariah Schneider <zechsch@umich.edu>
2017-03-05 15:41:57 -0500

    added voting endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 import os
@@ -16,19 +16,9 @@ heroku = Heroku(app)
 # Set "homepage" to index.html
 @app.route('/')
 def index():
-    return redirect("http://zechsch.github.io", code=302)
-    return render_template('index.html')
+    return redirect("http://botl-app.com", code=302)
 
 """
-JSON object for request:
-{
-    "latitude": Lat,
-    "longitude": Long,
-    "message": msg,
-    "thread_id": thread,
-    "user_id": uid,
-}
-
 Requires user_id to be in users table.
 Should be taken care of on install and first open.
 Adds post to posts database."""
@@ -36,25 +26,59 @@ Adds post to posts database."""
 def new_post():
     post = None
     if request.method != 'POST':
-        return
+        return make_response(jsonify({'status': 'failed',
+                                      'error': 'Method not allowed'}), 405)
     try:
         data = request.get_json()
         latitude = data['latitude']
         longitude = data['longitude']
         msg = data['message']
-        thread = data['thread_id']
         user = data['user_id']
 
-        db.engine.execute("insert into Posts(Latitude, Longitude, Message, ThreadId, UserId) values(" +
-            str(latitude) + ", " + str(longitude) + ", \'" + msg + "\', " + str(thread) + ", " + str(user) + ");")
+        db.engine.execute("insert into Posts(Latitude, Longitude, Message, UserId) values(" +
+            str(latitude) + ", " + str(longitude) + ", \'" + msg + "\', " + str(user) + ");")
 
         return jsonify({'status': 'OK'})
     except:
 
-        return jsonify({'status': 'failed',
-                        'error': str(sys.exc_info()[0])})
+        return make_response(jsonify({'status': 'failed',
+                        'error': str(sys.exc_info()[0])}), 500)
 
 
+
+#get posts
+#reply
+#rate
+"""{
+'post': postID,
+'vote': ['up'/'down']
+}"""
+@app.route('/api/rate_post', methods=['POST'])
+def reate_post():
+
+    if request.method != 'POST':
+        print(request.method)
+        return make_response(jsonify({'status': 'failed',
+                                      'error': 'Method not allowed'}), 405)
+    try:
+        data = request.get_json()
+        post_id = data['post']
+        vote = data['vote']
+
+        result = db.engine.execute("select rating from Posts where PostID=" + str(post_id) + ";")
+
+        for row in result:
+            rating = row[0]
+        if vote == 'up':
+            rating += 1
+        else:
+            rating -= 1
+
+        db.engine.execute("update Posts set rating=" + str(rating) + " where PostID=" + str(post_id) + ";")
+
+        return make_response(jsonify({'status': 'ok'}, 200))
+    except:
+        return
 
 if __name__ == '__main__':
     app.debug = True
