afa61e87daf7fd80f3ee68066a5d0dc6616afdcf
Zechariah Schneider <zechsch@umich.edu>
2017-03-17 14:48:51 -0400

    added delete trigger based on time

--- PostsInit.sql
+++ PostsInit.sql
@@ -1,7 +1,9 @@
 DROP TABLE ArchivedPosts;
 DROP TABLE Posts;
-Drop sequence "tid";
-Drop sequence "pid";
+Drop sequence tid;
+Drop sequence pid;
+Drop function delete_old_posts() cascade;
+
 
 create sequence tid start 1;
 create sequence pid start 1;
@@ -17,7 +19,7 @@ CREATE TABLE Posts (
     Rating      integer NOT NULL default 0,
     UserID      integer NOT NULL,
     Uts         timestamp without time zone NOT NULL default (now() at time zone 'utc'),
-    ExpiredDate timestamp without time zone NOT NULL default (now() at time zone 'utc' + interval '12 hours')
+    ExpiredDate timestamp without time zone NOT NULL default (now() at time zone 'utc' + interval '24 hours')
 );
 
 CREATE TABLE ArchivedPosts (
@@ -31,5 +33,18 @@ CREATE TABLE ArchivedPosts (
     Rating      integer NOT NULL,
     UserID      integer NOT NULL,
     Uts         timestamp without time zone NOT NULL default (now() at time zone 'utc'),
-    ExpiredDate timestamp without time zone NOT NULL default (now() at time zone 'utc' + interval '12 hours')
+    ExpiredDate timestamp without time zone NOT NULL default (now() at time zone 'utc' + interval '24 hours')
 );
+
+CREATE FUNCTION delete_old_posts() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  DELETE FROM posts WHERE NOW() at time zone 'utc' > ExpiredDate;
+  RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER delete_trigger
+    AFTER INSERT ON posts
+    EXECUTE PROCEDURE delete_old_posts();
